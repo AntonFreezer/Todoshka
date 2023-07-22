a5a6acebac4886f92b7953fff800137c9099f45f
AntonFreezer <anton.kholodkov@gmail.com>
2023-07-22 13:22:50 +0300

    Initial Commit

---
+++ Todoshka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoshka
+//
+//  Created by Anton Kholodkov on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
